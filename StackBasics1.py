3a94dd4640d9c8ff6187cf29848c088efed6db79
RooiBos-Dev <30982136+RooiBos-Dev@users.noreply.github.com>
2020-02-07 15:23:59 +0200

    Added Main method. Added getStack() and isEmpty()

--- StackBasics1.py
+++ StackBasics1.py
@@ -5,6 +5,23 @@ class Stack():
         self.items.append(item)
     def pop(self):
         return self.items.pop()
+    def getStack(self):
+        print(self.items)
+    def isEmpty(self):
+        return self.items == []
 
+def main():
+    obj = Stack()
+    obj.push("Cal")
+    obj.push("Dylan")
+    obj.push("Yusrie")
+    obj.push("Eric")    
+    obj.getStack()
+    obj.pop()
+    obj.pop()
+    obj.getStack()
 
-    
+    print(obj.isEmpty())
+
+if __name__ == '__main__':
+    main()
